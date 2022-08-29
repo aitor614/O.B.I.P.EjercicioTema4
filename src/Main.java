e833008c0455b66a1b08b57393eeb3b021527f86
Aitor Sanchez Cervantes <aitor614@gmail.com>
2022-08-29 22:48:53 +0200

    Bucle While

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int numeroIf = 0;
+        int numeroWhile = 0;
+        int numeroDoWhile =0;
+
+        System.out.println("CONDICION IF"); //Para hacer agradable la vista en el teminal.
 
         if (numeroIf > 0) {
             System.out.println("El numero " + numeroIf + " es positivo");
@@ -9,6 +13,13 @@ public class Main {
             System.out.println("El numero " + numeroIf + " es negativo");
         } else {
             System.out.println("El numero es 0");
+        }
+
+        System.out.println("--------------- \n BUCLE WHILE"); //Para hacer agradable la vista en el teminal.
+
+        while (numeroWhile < 3 ) {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
         }
     }
 }
